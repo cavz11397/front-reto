40586c092add3d19d5e42353918518d43871bd01
cristian <cristian.vasquez@sofka.com.co>
2025-01-16 11:50:30 -0500

    despliegue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.tu-dominio.com/items/', // URL de la API para producción
+    apiUrl: 'https://back-reto.onrender.com/', // URL de la API para producción
     // Otros valores de configuración específicos para producción
   };
